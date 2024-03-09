15dd6951806ffb8e5660fd5015e9bebc5863982a
SeymaAtmaca <symatmc@hotmail.com>
2024-03-10 01:09:50 +0300

    first commit

---
+++ src/preload.js
@@ -0,0 +1,6 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('api', {
+    title : "The Note App",
+    createNote : (data) => ipcRenderer.invoke('create-file', data)
+})
